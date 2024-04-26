2887e020cf5c9dabf68cf54e169b674f572e9bd2
Lucas <lucasamarale@hotmail.com>
2024-04-26 15:01:43 -0300

    feat: terminando implementacao das classes do desafio e realizando testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123abc", "nokia1", "1av24ac", 8);
+Console.WriteLine("Nokia:");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine("");
+
+Iphone iphone = new Iphone("321cba", "iphone1", "123zvnn12", 8);
+Console.WriteLine("Iphone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
